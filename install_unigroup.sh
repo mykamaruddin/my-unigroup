2458f17a847eb6e51e7f6fa3e6b786db076e2fa5
mykamaruddin <myusni.kamaruddin@gmail.com>
2025-04-18 19:45:31 +0800

    Update install_unigroup.sh
    Change script initiation

--- install_unigroup.sh
+++ install_unigroup.sh
@@ -10,7 +10,7 @@ echo "------------------------------------------------------------"
 echo "Step 1: Checking for root permissions..."
 echo "------------------------------------------------------------"
 if [[ $EUID -ne 0 ]]; then
-   echo "❌ This script must be run as root. Try: sudo ./setup_myk_nodes.sh"
+   echo "❌ This script must be run as root. Try: sudo ./install_unigroup.sh"
    exit 1
 fi
 clear
